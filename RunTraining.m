e4e401a0b345066fd40dbe7d0461eebb65c895bf
Alex Yuan <aysz88@gmail.com>
2014-07-22 14:12:55 -0400

    Some bug fixes

--- RunTraining.m
+++ RunTraining.m
@@ -11,23 +11,30 @@ end
 screenCenter = 0.5*(HW.screenRect([1 2]) + HW.screenRect([3 4]));
 
 screenSize = HW.screenRect([3 4]) - HW.screenRect([1 2]);
-screenDiag = sqrt(sum(screenSize .* screenSize));
-screenDiagDeg = screenDiag / HW.ppd; % TODO use tangent? Though error is low
+screenDiagPx = sqrt(sum(screenSize .* screenSize));
+screenDiagDeg = screenDiagPx / HW.ppd; % TODO use tangent? Though error is low
 
-sizesChoices = 0.75 * 2.^[0 1 2 3]; % sigma in arcmin at fovea
-separation = 6; % Number of sigma between the centers of the gabors
+fixWidthPx = 0.02*screenSize(1);
+fixLineWidthPx = 2;
 
+sizeChoices = 10 * 2.^[0 1 2 3]; % sigma in arcmin at fovea
+separation = 3; % Number of sigma between the centers of the gabors
+
+% Calculate how much "cortical radius" the screen fills (equiv. visual
+% angle at fovea)
 % From CorticalMag, r_VisSpace = E_h * (exp(r_cortex/E_h) - 1);
-% Thus the cortical radius to fill the screen is E_h*ln(r/E_h + 1)
+% Thus the cortical radius to fill the screen is E_h*ln(r_VisSpace/E_h + 1)
 E_h = 2.5;
 corticalRadiusDeg = E_h*log(screenDiagDeg/E_h + 1);
 corticalR_AM = corticalRadiusDeg * 60;
+
+% Calculate coordinates of stimuli
 hexCoordsDeg = [];
 hexSizesAM = [];
-for sizeIdx = 1:length(sizesChoices)
-    thisSize = sizesChoices(sizeIdx);
+for sizeIdx = 1:length(sizeChoices)
+    thisSize = sizeChoices(sizeIdx);
     nTilesAcross = corticalR_AM/(thisSize*separation);
-    thisSizeCoordsDeg = HexLattice(nTilesAcross) * thisSize/60;
+    thisSizeCoordsDeg = HexLattice(nTilesAcross) * thisSize/60 * separation;
     nTheseCoords = size(thisSizeCoordsDeg, 1);
     hexCoordsDeg = [hexCoordsDeg; thisSizeCoordsDeg];
     hexSizesAM = [hexSizesAM; thisSize*ones(nTheseCoords, 1)];
@@ -47,8 +54,6 @@ zones = 1:9;
 zoneXs = mod(zones-1, 3);           % [0 1 2 0 1 2 0 1 2]
 zoneYs = 3 - floor((zones-1)./3);   % [0 0 0 1 1 1 2 2 2]
 
-nGabors = 20;
-
 iters = 1e6;
 
 try
@@ -65,9 +70,12 @@ try
 %         gaborCenters = rand(nGabors, 2) .* repmat(screenSize, nGabors, 1);
 %         sizes = sizesChoices(randi(length(sizesChoices), nGabors, 1)) * HW.ppd ./ 60;
 %         sizes = sizes' .* pdist2(gaborCenters, presCenter).^0.7;
-        coordIndexes = randi(nMagCoords, 1, nGabors);
+        %coordIndexes = randi(nMagCoords, 1, nGabors);
+        coordIndexes = (abs(hexSizesAM - sizeChoices(randi(length(sizeChoices)))) < 1e-5);
         gaborCenters = magCoordsPx(coordIndexes, :);
-        sizes = magCoordScalesPx(coordIndexes);
+        nGabors = size(gaborCenters, 1);
+        
+        sizes = magCoordScalesPx(coordIndexes); % FIXME this is the number of pixels for a cycle, not the whole texture
         ySizes = sizes .* 1.0./(aRatioMin + rand(nGabors,1).*(aRatioMax - aRatioMin));
         offsets = 0.5*[sizes ySizes]; % offsets of each texture away from the centers of each gabor
         texDestPos = [gaborCenters - offsets, gaborCenters + offsets];
@@ -75,9 +83,6 @@ try
         
         lums = lumMin + rand(nGabors,1).*(lumMax - lumMin);
         texColors = HW.white .* cat(2, lums, lums, lums);
-        
-        fixWidthPx = 0.02*screenSize(1);
-        fixLineWidthPx = 2;
         
         for eye = [0 1]
             HW = ScreenCustomStereo(...
