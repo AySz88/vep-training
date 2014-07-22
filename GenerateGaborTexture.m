12018e1a0194dbcc1912d5464810d3fe0e08e6ca
Alex Yuan <AySz88@gmail.com>
2014-07-21 20:23:49 -0400

    Commit missing file

---
+++ GenerateGaborTexture.m
@@ -0,0 +1,25 @@
+function [ HW, gaborTexture ] = GenerateGaborTexture( HW, sigma, gamma, lambda, phi, textureRes )
+%GENERATEGABORTEXTURE Generates a gabor texture
+% Detailed explanation goes here
+%     sigma = size of gabor
+%     gamma = aspect ratio
+%     lambda = wavelength of carrier
+%     phi = phase shift
+% from Gabor2D
+    if nargin < 2 % TODO check each of these argument individually
+        sigma = pi;
+        gamma = 1.0;
+        lambda = 2*pi;
+        phi = 0.0;
+        textureRes = 512; % for the texture of the basic gabor
+    end
+    
+    [X, Y] = meshgrid(-4*pi:(8*pi)/(textureRes-1):4*pi);
+    Z = exp(-(X.^2 + gamma^2 * Y.^2)/(2*sigma^2)) .* cos(2*pi*X/lambda + phi);
+
+    HW = ScreenCustomStereo(...
+        HW, 'SelectStereoDrawBuffer', HW.winPtr, 0);
+    [HW, gaborTexture] = ScreenCustomStereo(...
+        HW, 'MakeTexture', HW.realWinPtr, Z, [], [], 1); %FIXME hack realwinptr
+end
+
